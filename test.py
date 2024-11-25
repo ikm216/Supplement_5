b4bc96377fa58f1ebcf744ff25091921a5f6fe2c
ikm216 <ikmclassof22@gmail.com>
2024-11-25 18:35:16 -0500

    test: Added test to check calculating the square root and check if it is a negative number return a exception.

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-def test():
-    pass
+import pytest
 
-test()
+def test_should_return_3_from_9():
+    assert square_root(9) == 3
+
+def test_should_return_exception_from_negative_num():
+    with pytest.raises(ValueError, match = "A negative number cannot be calutaed to get square root"):
+        square_root(-9)
