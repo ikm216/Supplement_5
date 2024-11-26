efbb378fd2a74a8982c1e66960292d8f45afb9ba
ikm216 <ikmclassof22@gmail.com>
2024-11-25 19:05:28 -0500

    test: Added four tests to test the requirements of the random number.

--- test.py
+++ test.py
@@ -22,3 +22,15 @@ def test_should_return_3_from_9():
 def test_should_return_exception_from_negative_num():
     with pytest.raises(ValueError, match = "A negative number cannot be calutaed to get square root"):
         square_root(-9)
+
+def test_should_return_nothing_from_2_even():
+    assert random_num(2) is True
+
+def test_should_return_6_from_3_odd():
+    assert random_num(3) == 6
+
+def test_should_return_4_from_12_divisble_three():
+    assert random_num(12) == 4
+
+def test_should_return_3_from_12_divisble_four():
+    assert random_num(12) == 3
