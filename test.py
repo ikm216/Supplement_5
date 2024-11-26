ecd4737fc2fc00682f24f39d518ba5292feefcc1
ikm216 <ikmclassof22@gmail.com>
2024-11-25 19:38:41 -0500

    test: Revamp the first test to help accept an argument to test the function.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import math
 import pytest
+import random
 
 def square_root(num):
     """
@@ -16,6 +17,11 @@ def square_root(num):
     else:
         raise  ValueError("A negative number cannot be calutaed to get square root")
     
+
+
+    
+
+#Test for square_root    
 def test_should_return_3_from_9():
     assert square_root(9) == 3
 
@@ -23,8 +29,9 @@ def test_should_return_exception_from_negative_num():
     with pytest.raises(ValueError, match = "A negative number cannot be calutaed to get square root"):
         square_root(-9)
 
+#Test for random_num
 def test_should_return_nothing_from_2_even():
-    assert random_num(2) is True
+    assert random_num(2) == 2
 
 def test_should_return_6_from_3_odd():
     assert random_num(3) == 6
@@ -34,3 +41,7 @@ def test_should_return_4_from_12_divisble_three():
 
 def test_should_return_3_from_12_divisble_four():
     assert random_num(12) == 3
+
+def test_should_return_exception_from_6():
+    with pytest.raises(ValueError, match = "The number is greater than 4"):
+        random_num(6)
